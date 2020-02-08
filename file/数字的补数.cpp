b1d6d96196737dffbb7cd13a114dc8ebc7998935
寒月老大 <48745208+hanyuelaoda@users.noreply.github.com>
2020-02-08 16:22:44 +0800

    Create 数字的补数.cpp

---
+++ file/数字的补数.cpp
@@ -0,0 +1,32 @@
+//两种方法的思路都一样
+//方法1则是用pow函数，方法2用左移完成
+class Solution1
+{
+public:
+	int findComplement(int num) 
+	{
+		int count = 0;
+		int tmp = num;
+		while (tmp)
+		{
+			count++;
+			tmp /= 2;
+		}
+		return pow(2, count) - num - 1;
+	}
+};
+
+class Solution 
+{
+public:
+	int findComplement(int num) 
+	{
+  //此处需要long类型，否则会溢出
+		long tmp = 1;
+		while (num>=tmp)
+		{
+			tmp <<= 1;
+		}
+		return tmp - num - 1;
+	}
+};

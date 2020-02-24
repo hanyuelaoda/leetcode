4a90a8c120e63e4c93362475ad9b4548eb6c2400
寒月老大 <48745208+hanyuelaoda@users.noreply.github.com>
2020-02-24 21:48:33 +0800

    Create 判断字符串中字符是否唯一.cpp

---
+++ file/判断字符串中字符是否唯一.cpp
@@ -0,0 +1,16 @@
+class Solution 
+{
+public:
+	bool isUnique(string astr) 
+	{
+		set<char> m_set;
+		for (char c : astr)
+		{
+			if (!m_set.insert(c).second)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+};
